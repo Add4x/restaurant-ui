9f7e61938c798f71221ea057d5978a986f72ee7f
shyju.viswambaran <shyjuezy@gmail.com>
2024-12-28 23:47:42 -0600

    Fixed the menu close while re-orientation

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import NavItem from "./NavItem";
