be958696e807eaee47089c899d4ee15ace04a870
shyju.viswambaran <shyjuezy@gmail.com>
2024-12-28 23:45:39 -0600

    Fixed the menu close while re-orientation

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import NavItem from "./NavItem";
@@ -32,7 +32,7 @@ const MobileMenu = ({ isOpen, setIsMobileMenuOpen }: MobileMenuProps) => {
       window.removeEventListener("orientationchange", closeHamburgerMenu);
       window.removeEventListener("resize", closeHamburgerMenu);
     };
-  }, []);
+  }, [setIsMobileMenuOpen]);
 
   if (!isOpen) return null;
 
