213154d2ae1edd450bda7b5cb79215132938d7da
Shyju Viswambaran <shyjuezy@gmail.com>
2025-01-04 14:08:55 -0600

    Add TODO comment for LoadingGrid component to implement loading skeletons

--- src/components/loading-grid.tsx
+++ src/components/loading-grid.tsx
@@ -2,6 +2,8 @@ interface LoadingGridProps {
   count?: number;
 }
 
+// TODO: Add a loading grid component that can be used to display a grid of loading skeletons
+
 export function LoadingGrid({ count = 4 }: LoadingGridProps) {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
