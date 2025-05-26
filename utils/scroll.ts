ecdc521f7f8eca3cf2581dad917ca72902651866
Shyju Viswambaran <shyjuezy@gmail.com>
2025-05-26 13:53:10 -0500

    Remove unused components and utility functions to streamline the codebase
    - Deleted the `layout-bkp.tsx`, `error-boundary.tsx`, `error.tsx`, `jwt-client.ts`, `create-slug.ts`, and `scroll.ts` files as they are no longer needed.
    - This cleanup improves maintainability and reduces clutter in the project.

--- utils/scroll.ts
+++
@@ -1,6 +0,0 @@
-export function scrollToElement(elementId: string) {
-  const element = document.getElementById(elementId);
-  if (element) {
-    element.scrollIntoView({ behavior: "smooth" });
-  }
-}
